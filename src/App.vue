ea8a91263a78391c026e9600ec80454775ee4b7b
ejimenez123 <caribestic123@gmail.com>
2021-12-14 16:39:59 -0500

    feat: My first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <full-calendar :events="events"></full-calendar>
+  </div>
+</template>
+
+<script>
+//https://github.com/CroudTech/vue-fullcalendar
+import { FullCalendar } from 'vue-full-calendar'
+import 'fullcalendar/dist/fullcalendar.css'
+
+export default {
+  components: {
+    FullCalendar,
+  },
+  data() {
+    return {
+      events: [
+        {
+            title  : 'event1',
+            start  : '2021-12-01',
+            color: 'yellow',
+          textColor: 'black',
+        },
+        {
+            title  : 'event2',
+            start  : '2021-12-05',
+            end    : '2021-12-07',
+                      color: 'red',
+        },
+        {
+            title  : 'event3',
+            start  : '2021-12-09T12:30:00',
+            allDay : false,
+        },
+      ]
+    }
+  }
+}
+</script>
+
